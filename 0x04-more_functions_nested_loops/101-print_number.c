6bf0593962b132208c679e081e08395053cd54be
Omohlara123 <omoh_lara88@ymail.com>
2023-05-25 14:51:14 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,22 +2,24 @@
 
 /**
  * print_number - prints an integer
- * @n: input integer parameter
- *
- * Return: Always 0
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -i;
-	}
-	if (i / 10)
+		n1 = -n;
+		_putchar('_');
+	} else
 	{
-		print_number(i / 10);
+		n1 = n;
 	}
-	_putchar(i % 10 + '0');
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
