e2d047f49528d4bf1978c3861c386aa66c52259a
Omohlara123 <omoh_lara88@ymail.com>
2023-05-25 07:00:12 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,6 @@
 /**
  * more_numbers - print more numbers
  */
-
 void more_numbers(void)
 {
 	int i, j;
@@ -14,7 +13,7 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 				_putchar('1');
-			_putchar (j % 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
