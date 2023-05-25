83351fad8b021bcd5a6351068980c7c3253ff33e
Omohlara123 <omoh_lara88@ymail.com>
2023-05-25 12:54:59 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * more_numbers - prints numbers to 14
- *
- * Return: returns nothing
+ * more_numbers - print more number
  */
 
 void more_numbers(void)
 {
-	int n1, n2;
+	int i, j;
 
-	for (n1 = 0; n1 < 10; n1++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (n2 = 0; n2 <= 14; n2++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (n2 > 9)
-			{
-				putchar((n2 / 10) + '0');
-			}
-			putchar((n2 % 10) + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
-		putchar(10);
+		_putchar('\n');
 	}
 }
